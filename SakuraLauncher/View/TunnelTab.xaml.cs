210f6fa4346fe20b39c76dc8485788edbb830209
fengberd <admin@berd.moe>
2020-02-13 19:38:42 +0800

    Now you can delete tunnel

--- SakuraLauncher/View/TunnelTab.xaml.cs
+++ SakuraLauncher/View/TunnelTab.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 
+using SakuraLauncher.Data;
+
 namespace SakuraLauncher.View
 {
     public partial class TunnelTab : UserControl
@@ -14,5 +16,27 @@ namespace SakuraLauncher.View
         }
 
         private void ButtonAddListener_Click(object sender, RoutedEventArgs e) => new CreateTunnelWindow().ShowDialog();
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if((sender as Button).DataContext is Tunnel tunnel && tunnel.IsReal)
+            {
+                if(MessageBox.Show("是否确定删除隧道 " + tunnel.Name + "?\n该操作不可撤销.", "Confirm", MessageBoxButton.OKCancel, MessageBoxImage.Asterisk) != MessageBoxResult.OK)
+                {
+                    return;
+                }
+                IsEnabled = false;
+                App.ApiRequest("deletetunnel", "name=" + tunnel.Real.Name + "&server=" + tunnel.Real.ServerID).ContinueWith(t => Dispatcher.Invoke(() =>
+                {
+                    IsEnabled = true;
+                    var json = t.Result;
+                    if(json == null)
+                    {
+                        return;
+                    }
+                    Main.Tunnels.Remove(tunnel);
+                }));
+            }
+        }
     }
 }
