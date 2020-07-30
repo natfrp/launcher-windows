12a52f6af87721269ea597cdf720df34ae44ca39
fengberd <admin@berd.moe>
2020-07-30 19:41:04 +0800

    Bump version to 1.0.3.0

--- LegacyLauncher/Properties/AssemblyInfo.cs
+++ LegacyLauncher/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("SakuraFrp Legacy Launcher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SakuraFrp 传统启动器")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SakuraFrp Legacy Launcher")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.4")]
-[assembly: AssemblyFileVersion("1.0.2.4")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
--- SakuraLauncher/Properties/AssemblyInfo.cs
+++ SakuraLauncher/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("SakuraFrp Launcher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SakuraFrp WPF启动器")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SakuraFrp Launcher")]
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.4")]
-[assembly: AssemblyFileVersion("1.0.2.4")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
