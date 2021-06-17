a0fd21b079ea3da490788418d89ca69269895cc0
fengberd <admin@berd.moe>
2021-06-17 09:43:43 +0800

    Service.macOS: Avoid duplicate instances

--- SakuraFrpService.macOS/Main.cs
+++ SakuraFrpService.macOS/Main.cs
@@ -1,4 +1,7 @@
-﻿using AppKit;
+﻿using System.Linq;
+using System.Diagnostics;
+
+using AppKit;
 using Foundation;
 
 using SakuraFrpService.Provider;
@@ -10,6 +13,23 @@ namespace SakuraFrpService
         static void Main(string[] args)
         {
             NSApplication.Init();
+
+            var list = NSWorkspace.SharedWorkspace.RunningApplications
+                .Where(app => app.BundleIdentifier == "moe.berd.SakuraLauncher.Service" && app.ProcessIdentifier != Process.GetCurrentProcess().Id)
+                .Select(app => app.ProcessIdentifier)
+                .ToArray();
+
+            if (list.Length != 0)
+            {
+                var alert = new NSAlert()
+                {
+                    AlertStyle = NSAlertStyle.Warning,
+                    InformativeText = "找到 " + list.Length + " 个重复进程，PID = [" + string.Join(',', list) + "]",
+                    MessageText = "请勿重复开启 SakuraFrp Service"
+                };
+                return;
+            }
+
             new SakuraService(new ConfigProvider(), new UtilsProvider(), new CommunicationProvider(NSFileManager.DefaultManager.GetContainerUrl("moe.berd.SakuraL").Path + "/Library/Caches"), new SodiumProvider()).Start(false);
             NSApplication.Main(args);
         }
